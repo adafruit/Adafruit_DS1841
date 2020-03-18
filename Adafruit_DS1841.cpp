050ad1ef616afcb73fb86192e540190c3fff7b1d
siddacious <nospam187+github@gmail.com>
2020-03-17 17:24:37 -0700

    initial commit

---
+++ Adafruit_DS1841.cpp
@@ -0,0 +1,126 @@
+/*!
+ *  @file Adafruit_DS1841.cpp
+ *
+ *  @mainpage Adafruit DS1841 I2C Logorithmic Resistor
+ *
+ *  @section intro_sec Introduction
+ *
+ * 	I2C Driver for the Library for the DS1841 I2C Logorithmic Resistor
+ *
+ * 	This is a library for the Adafruit DS1841 breakout:
+ * 	https://www.adafruit.com/product/45XX
+ *
+ * 	Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit and open-source hardware by purchasing products from
+ * 	Adafruit!
+ *
+ *  @section dependencies Dependencies
+ *  This library depends on the Adafruit BusIO library
+ *
+ *  This library depends on the Adafruit Unified Sensor library
+ *
+ *  @section author Author
+ *
+ *  Bryan Siepert for Adafruit Industries
+ *
+ * 	@section license License
+ *
+ * 	BSD (see license.txt)
+ *
+ * 	@section  HISTORY
+ *
+ *     v1.0 - First release
+ */
+
+#include "Arduino.h"
+#include <Wire.h>
+
+#include "Adafruit_DS1841.h"
+
+/**
+ * @brief Construct a new Adafruit_DS1841::Adafruit_DS1841 object
+ *
+ */
+Adafruit_DS1841::Adafruit_DS1841(void) {}
+
+/*!
+ *    @brief  Sets up the hardware and initializes I2C
+ *    @param  i2c_address
+ *            The I2C address to be used.
+ *    @param  wire
+ *            The Wire object to be used for I2C connections.
+ *    @param  sensor_id
+ *            The unique ID to differentiate the sensors from others
+ *    @return True if initialization was successful, otherwise false.
+ */
+bool Adafruit_DS1841::begin_I2C(uint8_t i2c_address, TwoWire *wire) {
+
+  i2c_dev = new Adafruit_I2CDevice(i2c_address, wire);
+
+  if (!i2c_dev->begin()) {
+    return false;
+  }
+
+  return _init();
+}
+
+/*!  @brief Initializer for post i2c/spi init
+ *   @param sensor_id Optional unique ID for the sensor set
+ *   @returns True if chip identified and initialized
+ */
+bool Adafruit_DS1841::_init(void) {
+
+  // make sure we're talking to the right chip
+  // still need to find a way to verify
+
+  // self._eeprom_shadow_bit = True # turn off eeprom updates to IV
+
+  Adafruit_BusIO_Register config_1 = Adafruit_BusIO_Register(i2c_dev, DS1841_CR1, 1);
+  Adafruit_BusIO_Register config_2 = Adafruit_BusIO_Register(i2c_dev, DS1841_CR2, 1);
+
+  config_1.write(0x2); // Addr mod=False, Update mode = True
+  config_2.write(0x6); // LUTAR mode = manual, Wiper Access = manual/i2c
+  return true;
+}
+
+// _voltage_register = UnaryStruct(_DS1841_VOLTAGE, ">B")
+
+/**
+ * @brief Gets the current wiper value
+ *
+ * @return uint8_t The current 7-bit wiper value
+ */
+uint8_t Adafruit_DS1841::getWiper(void) {
+
+  Adafruit_BusIO_Register wiper_register = Adafruit_BusIO_Register(
+      i2c_dev, DS1841_WR, 1);
+  return wiper_register.read();
+}
+/**
+ * @brief Sets the rate at which pressure and temperature measurements
+ *
+ * @param wiper_value The 7-bit number to set the wiper value to
+ */
+bool Adafruit_DS1841::setWiper(uint8_t wiper_value) {
+  if (wiper_value > 127){
+    return false;
+  }
+  Adafruit_BusIO_Register wiper_register = Adafruit_BusIO_Register(
+      i2c_dev, DS1841_WR, 1);
+
+  wiper_register.write(wiper_value);
+  return true;
+}
+
+/**
+ * @brief Gets the current temperature
+ *
+ * @return int8_t The current temperature in degrees C
+ */
+int8_t Adafruit_DS1841::getTemperature(void) {
+
+  Adafruit_BusIO_Register temperature_register = Adafruit_BusIO_Register(
+      i2c_dev, DS1841_TEMP, 1);
+
+  return (int8_t)temperature_register.read();
+}
---
+++ Adafruit_DS1841.h
@@ -0,0 +1,61 @@
+/*!
+ *  @file Adafruit_DS1841.h
+ *
+ * 	I2C Driver for the Adafruit DS1841 I2C Logorithmic Resistor
+ *
+ * 	This is a library for the Adafruit DS1841 breakout:
+ * 	https://www.adafruit.com/products/45XX
+ *
+ * 	Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit and open-source hardware by purchasing products from
+ * 	Adafruit!
+ *
+ *
+ *	BSD license (see license.txt)
+ */
+
+#ifndef _ADAFRUIT_DS1841_H
+#define _ADAFRUIT_DS1841_H
+
+#include "Arduino.h"
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_I2CDevice.h>
+#include <Wire.h>
+
+#define DS1841_IVR 0x00
+#define DS1841_CR0 0x02
+#define DS1841_CR1 0x03
+#define DS1841_LUTAR 0x08
+#define DS1841_WR 0x09
+#define DS1841_CR2 0x0A
+#define DS1841_TEMP 0x0C
+#define DS1841_VOLTAGE 0x0E
+// DS1841_LUT 0x80–C7h
+
+#define DS1841_VCC_LSB = 25.6
+#define DS1841_I2CADDR_DEFAULT = 0x28 // up to 0x2B
+
+class Adafruit_DS1841;
+
+/*!
+ *    @brief  Class that stores state and functions for interacting with
+ *            the DS1841 I2C Logorithmic Resistor
+ */
+class Adafruit_DS1841 {
+public:
+
+  bool begin_I2C(uint8_t i2c_addr = DS1841_I2CADDR_DEFAULT,
+                 TwoWire *wire = &Wire);
+
+  int8_t getTemperature(void);
+  uint8_t getWiper(void);
+  bool setWiper(uint8_t new_wiper_value);
+  void setWiperDefault(uint8_t new_wiper_default);
+
+private:
+  bool _init(void);
+  Adafruit_I2CDevice *i2c_dev = NULL; ///< Pointer to I2C bus interface
+
+};
+
+#endif
